b86f19116c88391abab8f3651ed2264eb8da8432
Diana Carolina Cuenca Zambrano <diana.cuenca.zambrano@udla.edu.ec>
2022-01-24 12:30:52 -0500

    La app ya funciona con la Api web

---
+++ LALCXamarin/LALCXamarin/LALCXamarin/Models/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LALC_UWP.Models
+{
+    public class Categoria
+    {
+
+        public int CategoriaID { get; set; }
+
+        public int UsuarioID { get; set; }
+        public string Nombre { get; set; }
+
+        public string Color { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public bool esPrioritaria { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+        public virtual ICollection<Subcategoria> Subcategorias { get; set; }
+
+    }
+}
---
+++ LALCXamarin/LALCXamarin/LALCXamarin/Models/Concepto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LALC_UWP.Models
+{
+    public class Concepto
+    {
+
+        public int ConceptoID { get; set; }
+
+        public int SubcategoriaID { get; set; }
+
+        public string Definicion { get; set; }
+
+        public string Titulo { get; set; }
+
+    }
+}
