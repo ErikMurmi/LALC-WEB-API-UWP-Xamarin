ddfb46c5e3d1c40550f653f54af8baf16aa6256c
Diana Carolina Cuenca Zambrano <diana.cuenca.zambrano@udla.edu.ec>
2022-01-24 11:45:33 -0500

    Se creó el archivo Xamarin

---
+++ LALCXamarin/LALCXamarin/LALCXamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using LALCXamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace LALCXamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
