406eb28f551ce639414e77ccf41f785185de8681
Diana Carolina Cuenca Zambrano <diana.cuenca.zambrano@udla.edu.ec>
2022-01-25 13:41:21 -0500

    Login Implementado

--- LALCXamarin/LALCXamarin/LALCXamarin/Views/LoginPage.xaml.cs
+++ LALCXamarin/LALCXamarin/LALCXamarin/Views/LoginPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -28,9 +29,47 @@ namespace LALCXamarin.Views
         public Func<HttpRequestMessage, X509Certificate2, X509Chain, SslPolicyErrors, Boolean>
             ServerCertificateCustomValidationCallback{ get; set; }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private bool comprobacionEma()
         {
+            if (string.IsNullOrEmpty(campoEmail.Text))
+            {
+                msEmail.Text = "Este campo no puede estar vacío";
+                return true;
+            }
+            if (!Regex.IsMatch(campoEmail.Text, @"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*"))
+            {
+                msEmail.Text = "Correo inválido";
+                return true;
+            }
+            else
+            {
+                msEmail.Text = string.Empty;
+                return false;
+            }
+        }
 
+        private bool comprobacionCon()
+        {
+            if (string.IsNullOrEmpty(campoContraseña.ToString())
+                || string.IsNullOrWhiteSpace(campoContraseña.ToString())
+                || campoContraseña.Text.Length < 8)
+            {
+                msContraseña.Text = "Campo inválido (min 8 caracteres)";
+                return true;
+            }
+            if (campoContraseña.Text.ToUpper() == "CONTRASEÑA" || campoContraseña.Text.ToUpper() == "PASSWORD")
+            {
+                msContraseña.Text = "'" + campoContraseña + "'" + " no es una contraseña válida.";
+                return true;
+            }
+            else
+            {
+                msContraseña.Text = string.Empty;
+                return false;
+            }
+        }
+        private async void getData()
+        {
             var httpHandler = new HttpClientHandler { ServerCertificateCustomValidationCallback = (o, cert, chain, errors) => true };
             HttpClient client = new HttpClient(httpHandler);
 
@@ -43,9 +82,30 @@ namespace LALCXamarin.Views
             {
                 string content = await response.Content.ReadAsStringAsync();
                 var resultado = JsonConvert.DeserializeObject<List<Usuario>>(content);
-                ListaDemo.ItemsSource = resultado;
-
+                foreach (var i in resultado)
+                {
+                    if (i.email.Equals(campoEmail.Text) && i.password.Equals(campoContraseña.Text))
+                    {
+                        App.actualUserId = i.UsuarioID;
+                        await Navigation.PushAsync(new Categorias());
+                    }
+                }
+                if (App.actualUserId == 0)
+                {
+                    msContraseña.Text = "Contraseña o email incorrecto";
+                }
             }
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!comprobacionEma() && !comprobacionCon())
+            {
+                getData();
+            }
+        }
+        private async Task IreRegistro_ClickedAsync(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Registro());
         }
     }
 }
