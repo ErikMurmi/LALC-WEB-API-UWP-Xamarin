3930c4b5baf4c3ef0976e30073f3a9a7e1e7703c
Erik <erikmurmi@gmail.com>
2022-01-28 14:33:08 -0500

    view model

---
+++ LALCXamarin/LALCXamarin/LALCXamarin/ViewModels/Categorias/CategoriasViewModel.cs
@@ -0,0 +1,29 @@
+﻿using LALC_UWP.Models;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using LALCXamarin.Views;
+using LALCXamarin.Services;
+
+namespace LALCXamarin.ViewModels
+{
+    public class CategoriasViewModel : Categoria
+    {
+
+        public LalcAPI lalcAPI { get; set; }
+        public String Title { get; set; }
+        public CategoriasViewModel()
+        {
+            Title = "Categorías";
+            lalcAPI = new LalcAPI();
+        }
+
+       /* public async  List<Categoria> OnAppearing()
+        {
+            var us = await lalcAPI.GetUsuario(App.actualUserId);
+            return us.Categorias;
+        }*/
+    }
+}
