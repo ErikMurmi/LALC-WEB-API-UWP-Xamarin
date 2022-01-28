1680c0ccbfaf0430f44d0b3871bb29192af324d5
Erik <erikmurmi@gmail.com>
2022-01-28 12:24:28 -0500

    ;

--- LALCXamarin/LALCXamarin/LALCXamarin/Views/LoginPage.xaml.cs
+++ LALCXamarin/LALCXamarin/LALCXamarin/Views/LoginPage.xaml.cs
@@ -87,7 +87,7 @@ namespace LALCXamarin.Views
                     if (i.email.Equals(campoEmail.Text) && i.password.Equals(campoContraseña.Text))
                     {
                         App.actualUserId = i.UsuarioID;
-                        await Navigation.PushAsync(new AppShell())
+                        await Navigation.PushAsync(new AppShell());
                     }
                 }
                 if (App.actualUserId == 0)
