3a0baee313f35eb7639354bd53162414a212246b
Erik <erikmurmi@gmail.com>
2022-01-29 22:15:44 -0500

    paddin en crear y editar , cambia nombre de sbcat

--- LALCXamarin/LALCXamarin/LALCXamarin/ViewModels/Subcategoria/CrearSubcategoriaViewModel.cs
+++ LALCXamarin/LALCXamarin/LALCXamarin/ViewModels/Subcategoria/CrearSubcategoriaViewModel.cs
@@ -6,6 +6,7 @@ using System.Text;
 using Xamarin.Forms;
 using LALCXamarin.Views;
 using LALCXamarin.Services;
+using LALCXamarin.Views.Subcategorias;
 
 namespace LALCXamarin.ViewModels
 {
@@ -25,7 +26,7 @@ namespace LALCXamarin.ViewModels
             var creada = await lalcAPI.CrearSubcategoria(NSubCategoria);
             if (creada)
             {
-                await Shell.Current.GoToAsync($"//{nameof(ItemsPage)}");
+                await Shell.Current.GoToAsync($"//{nameof(SubcategoriasView)}");
             }
 
         }
