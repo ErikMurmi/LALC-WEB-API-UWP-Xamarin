72fe532f78ed6df25a78de81047cbe8fa557ee7f
Diana Carolina Cuenca Zambrano <diana.cuenca.zambrano@udla.edu.ec>
2022-01-29 21:42:01 -0500

    se hicieron cambiso esteticos en el login

--- LALCXamarin/LALCXamarin/LALCXamarin/AssemblyInfo.cs
+++ LALCXamarin/LALCXamarin/LALCXamarin/AssemblyInfo.cs
@@ -3,3 +3,5 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("Dongle-Bold.ttf", Alias = "Lobster")]
+[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "Monserrate")]
+[assembly: ExportFont("OpenSans-SemiBold.ttf", Alias = "OpenSans")]
